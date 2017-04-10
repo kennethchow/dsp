c30a955f04456920afa0166536e5648447dc4876
Kenny Chow <RGD@Kenneths-MacBook-Pro-2.local>
2017-04-10 07:58:53 -0700

    final commit of prework

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,21 +1,41 @@
 import csv
-import re
+import re 
+from itertools import islice
 
-reader = csv.reader(open('/Users/RGD/ds/metis/metisgh/dsp/python/faculty.csv'), skipinitialspace=True)
-reader.__next__()
 
-names = []
-details = []
+with open('/Users/RGD/ds/metis/metisgh/dsp/python/faculty.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    faculty_dict = {rows[0]:[rows[1],rows[2],rows[3]] for rows in reader}
 
-for r in reader:
-	names.append(r[0])
-	details.append(r[1:4])
+def first_no(n, iterable): # Return first n items of the iterable as a list
+    return list(islice(iterable, n))
 
-listoflastnames = sorted(names, key=lambda x: x.split(" ")[-1])
+print(first_no(3, faculty_dict.items()))
 
-uniquelastnames = set([lastname.split()[-1] for lastname in listoflastnames])
 
-predic = {el:0 for el in uniquelastnames}
+with open('faculty.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    professor_dict = {}
+    for rows in reader:
+    	if len(rows[0].split(' ')) == 1:
+    		professor_dict.update({(rows[0].split(' ', 1)[0]):[rows[1],rows[2],rows[3]]})
+    	if len(rows[0].split(' ')) == 2:
+    		professor_dict.update({(rows[0].split(' ', 1)[0], rows[0].split(' ', 2)[1]):[rows[1],rows[2],rows[3]]})
+    	if len(rows[0].split(' ')) == 3:
+    		professor_dict.update({(rows[0].split(' ', 1)[0], rows[0].split(' ', 3)[2]):[rows[1],rows[2],rows[3]]})
 
-print(len(details))
-print(len(uniquelastnames))
+print(first_no(3, professor_dict.items()))
+
+with open('faculty.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    professor_dict = {}
+    for rows in reader:
+    	if len(rows[0].split(' ')) == 1:
+    		professor_dict.update({(rows[0].split(' ', 1)[0]):[rows[1],rows[2],rows[3]]})
+    	if len(rows[0].split(' ')) == 2:
+    		professor_dict.update({(rows[0].split(' ', 2)[1], rows[0].split(' ', 1)[0]):[rows[1],rows[2],rows[3]]})
+    	if len(rows[0].split(' ')) == 3:
+    		professor_dict.update({(rows[0].split(' ', 3)[2], rows[0].split(' ', 1)[0]):[rows[1],rows[2],rows[3]]})
+
+for key in sorted(professor_dict.keys(), key = lambda t:t[-1]):
+    print ("%s: %s" % (key, professor_dict[key]))
